54fb72a5e4ef76baa0f4f1fddd176d596e2e56ea
jVictor <victor.jvas1@gmail.com>
2021-06-20 01:44:22 -0300

    initial project

---
+++ Source/TheShooter/TheShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THESHOOTER_API ATheShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
